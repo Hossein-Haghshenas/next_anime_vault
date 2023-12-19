5a49e34c14b9392356d051c5b57c4e86ac025e98
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-12-19 23:02:48 +0330

    Return anime card instead of data

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -1,8 +1,10 @@
 "use server";
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 export const fetchAnime = async (page: number) => {
   // fetch data from api
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}?page=${page}&limit=8&order=popularity`);
   const data = await res.json();
-  return data;
+  return data.map((item: AnimeProp, index: number) => <AnimeCard key={item.id} anime={item} index={index} />);
 };
